66e4d755f609b465a8eb147c5918187edaae36dd
hd-44 <adxdxd@gmail.com>
2015-07-04 19:59:04 +0100

    Create editor.js

---
+++ src/editor.js
@@ -0,0 +1,215 @@
+function sl(id) { return document.getElementById(id); }
+var input=sl("editor");
+function opengoogle() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("http://www.google.com/search?q="+encode);
+ }
+function openhgoogle() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("http://www.google.com/search?tbs=qdr:h&q="+encode);
+ }
+function opendgoogle() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("http://www.google.com/search?tbs=qdr:d&q="+encode);
+ }
+function openwgoogle() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("http://www.google.com/search?tbs=qdr:w&q="+encode);
+ }
+function openmgoogle() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("http://www.google.com/search?tbs=qdr:m&q="+encode);
+ }
+function openygoogle() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("http://www.google.com/search?tbs=qdr:y&q="+encode);
+ }
+function openYoutube() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input);
+	MeinFenster = window.open("http://www.youtube.com/results?search_type=&search_query="+encode);
+  }
+function opennewtube() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("http://www.youtube.com/results?search_type=videos&search_sort=video_date_uploaded&search_query="+encode);
+ }
+function openvtube() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("http://www.youtube.com/results?search_type=videos&search_sort=video_view_count&search_query="+encode);
+ }
+function openntube() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("http://www.youtube.com/results?search_type=videos&search_sort=video_date_uploaded&search_query="+encode);
+ }
+function openltube() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("http://www.youtube.com/results?search_type=videos&search_sort=video_date_uploaded&filters=long&lclk=long&search_query="+encode);
+ }
+function opendtube() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("https://www.youtube.com/results?filters=today&lclk=today&search_query="+encode);
+ }
+function openwtube() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("https://www.youtube.com/results?filters=week&lclk=week&search_query="+encode);
+ }
+function openvdtube() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("https://www.youtube.com/results?filters=today&lclk=today&search_sort=video_view_count&search_query="+encode);
+ }
+function opendailymotion() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("http://www.dailymotion.com/ma/relevance/search/+encode/1");
+ }
+function openface() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input);
+	MeinFenster = window.open("http://www.facebook.com/search/results.php?q="+encode);
+  }
+function openppface() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("https://www.facebook.com/search/str/+encode+/keywords_pages/);
+ }
+function openpface() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("http://www.facebook.com/search/results.php?type=pages&q="+encode);
+ }
+function opengface() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("http://www.facebook.com/search/results.php?type=groups&q="+encode);
+ }
+function opentwitter() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("https://twitter.com/search/realtime?q=lang%3Aar+"+encode);
+ }
+function openntwitter() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("https://twitter.com/search?q=lang%3Aar+"+encode);
+ }
+function openytwitter() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("https://twitter.com/search?q=lang%3Aar%20site%3Ayoutube.com%20near%3A%22cairo%22%20within%3A%222500km%22&src=typd+"+encode);
+ }
+function openetwitter() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("https://twitter.com/search?q=lang%3Aar%20near%3A%22cairo%22%20within%3A%222500km%22&src=typd+"+encode);
+ }
+function openptwitter() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("https://twitter.com/search/users?q="+encode);
+ }
+function openyytwitter() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("https://twitter.com/search?q=lang%3Aar+site:youtube.com+"+encode);
+ }
+function openygtwitter() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("https://twitter.com/search?q=site:youtube.com+"+encode);
+ }
+function openvtwitter() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("https://twitter.com/search?q=site:vine.co+"+encode);
+ }
+function openvatwitter() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("https://twitter.com/search?q=lang%3Aar+site:vine.co+"+encode);
+ }
+function opentr1() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("https://translate.google.com/?source=osdd#ar/en/"+encode);
+ }
+function opentr2() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("https://translate.google.com/?source=osdd#en/ar/"+encode);
+ }
+function opentr3() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("https://translate.google.com/?source=osdd#ar/fr/"+encode);
+ }
+function opentr4() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("https://translate.google.com/?source=osdd#fr/ar/"+encode);
+ }
+function opentr5() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("https://translate.google.com/?source=osdd#ar/es/"+encode);
+ }
+function opentr6() {
+	var input=sl("editor").value;
+	var encode = encodeURI(input); 
+	MeinFenster = window.open("https://translate.google.com/?source=osdd#es/ar/"+encode);
+ }
+ 
+ $("#postToTwitter").bind("click",function(){
+
+                ga("send", "event", "Keyboard", "click", "Twitter");
+
+                document.getElementById("postToTwitter").href = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(textbox.value);
+
+                return true;
+            });
+
+            $("#postToFacebook").bind("click", function () {
+
+                ga("send", "event", "Keyboard", "click", "Facebook");
+
+                if (textbox.value.length > 0) {
+                    FB.login(function (response) {
+                        if (response.authResponse) {
+                            var publish = {
+                                method: 'stream.publish',
+                                message: textbox.value,
+                                user_prompt_message: 'Update your Facebook status'
+                            };
+                            FB.api(publish, function (response) {
+                                if (response && !response.error_msg) {
+                                    document.getElementById("response").innerHTML = "Message published to facebook.";
+                                } else {
+                                    document.getElementById("response").innerHTML = "Message not published to facebook.";
+                                }
+                            });
+                        }
+                    }, { scope: "publish_stream" });
+                }
+                else {
+                    document.getElementById("response").innerHTML = "Type a message in the box above before posting to facebook.";
+                }
+            });
+
+            $("#saveAsTextFile").bind("click", function () {
+                ga("send", "Keyboard", "Save", "send", textbox.value.length.toString());
+                setTimeout('document.location="./includes/save.php?text=' + encodeURIComponent(textbox.value) + '"', 100);
+                return false;
+            });
+     
